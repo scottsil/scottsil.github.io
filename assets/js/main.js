5f4e97c10c06f18e60c5f901c15a40743535ea48
Scott Silverman <scottsil@mac.com>
2014-04-23 01:51:59 -0700

    revert to class for typekit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
 $(window).load(function() {
-	$('div.solid').delay(1100).animate({opacity: 0}, 800, 'easeInOutQuad');
-	$('div.secondary').delay(1100).animate({opacity: 1}, 800, 'easeInOutQuad');
+	$('div.solid').delay(900).animate({opacity: 0}, 800, 'easeInOutQuad');
+	$('div.secondary').delay(900).animate({opacity: 1}, 800, 'easeInOutQuad');
 });
