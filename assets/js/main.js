7635be725d98ea4a1eb75c177d4c35818d3fc17b
Scott Silverman <scottsil@mac.com>
2014-04-23 01:48:46 -0700

    jquery animations on window load

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,4 @@
-
+$(window).load(function() {
+	$('div.solid').delay(1100).animate({opacity: 0}, 800, 'easeInOutQuad');
+	$('div.secondary').delay(1100).animate({opacity: 1}, 800, 'easeInOutQuad');
+});
